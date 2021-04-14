61ab13ae5b84f3de9a7791ce4aaa26d518302a63
Jago <jago.staes@gmail.com>
2021-04-14 15:37:21 +0200

    Added simple frontend to websocket application

---
+++ websocket-backend/src/main/resources/static/app.js
@@ -0,0 +1,40 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#jokelist").show();
+    }
+}
+
+function connect() {
+    var socket = new SockJS('/websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        stompClient.subscribe('/joke/jokestream', function (joke) {
+            showJoke(JSON.parse(joke.body).value);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+}
+
+function showJoke(joke) {
+    $("#jokes").append("<tr><td>" + joke + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+});
+
